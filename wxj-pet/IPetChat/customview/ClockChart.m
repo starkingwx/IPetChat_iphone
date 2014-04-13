e2c9d22b43396b5aa1c3e9c3b9058c00c2b03f96
starkingwx <starkingwx@gmail.com>
2014-04-13 22:33:17 +0800

    finish clock chart

--- wxj-pet/IPetChat/customview/ClockChart.m
+++ wxj-pet/IPetChat/customview/ClockChart.m
@@ -20,7 +20,15 @@
 #define STROKE_WDITH       20
 
 #define GAP         1
-#define FONT_SIZE   12
+#define FONT_SIZE   10
+#define SIDE_MARGIN 20
+
+#define ZERO_AM     @"0am"
+#define SIX_AM      @"6am"
+#define TWELVE_PM   @"12pm"
+#define SIX_PM      @"6pm"
+
+
 @interface ClockChart () {
     UIFont *_font;
 }
@@ -69,6 +77,12 @@
     CGFloat centerY = rect.size.height / 2;
     CGPoint center = CGPointMake(centerX, centerY);
 
+    CGFloat halfLineWidth = STROKE_WDITH / 2;
+    
+    CGFloat radius = rect.size.width < rect.size.height ? rect.size.width : rect.size.height;
+    radius /= 2;
+    radius -= (SIDE_MARGIN + GAP + halfLineWidth);
+    
     if (_motionStatArray) {
                        
         CGFloat start = ANGLE_START;
@@ -101,21 +115,23 @@
                     break;
             }
             
-            [self drawClockWithStartAngle:angleStart endAngle:angleEnd color:color center:center];
+            [self drawClockWithStartAngle:angleStart endAngle:angleEnd color:color center:center radius:radius];
             start = angleEnd;
         }
     } else {
-        [self drawClockWithStartAngle:ANGLE_START endAngle:ANGLE_END color:COLOR_OFFLINE center:center];
+        [self drawClockWithStartAngle:ANGLE_START endAngle:ANGLE_END color:COLOR_OFFLINE center:center radius:radius];
 
     }
     
+    [[UIColor blackColor] set];
+    // draw description text
     CGSize strSize = [self.descText sizeWithFont:_font];
     CGFloat halfStrWidth = strSize.width / 2;
     CGFloat tx1 = centerX - halfStrWidth;
     CGFloat ty1 = centerY - GAP - strSize.height;
-    [[UIColor blackColor] set];
     [self.descText drawAtPoint:CGPointMake(tx1, ty1) withFont:_font];
     
+    // draw point text
     NSString *pointText = [NSString stringWithFormat:@"%d/%d", _point, _maxPoint];
     strSize = [pointText sizeWithFont:_font];
     halfStrWidth = strSize.width / 2;
@@ -123,11 +139,40 @@
     CGFloat ty2 = centerY + GAP;
     [pointText drawAtPoint:CGPointMake(tx2, ty2) withFont:_font];
     
+    // draw time text
+    
+    // bottom
+    strSize = [ZERO_AM sizeWithFont:_font];
+    halfStrWidth = strSize.width / 2;
+    CGFloat tx3 = centerX - halfStrWidth;
+    CGFloat ty3 = centerY + radius + halfLineWidth + GAP;
+    [ZERO_AM drawAtPoint:CGPointMake(tx3, ty3) withFont:_font];
+    
+    // left
+    strSize = [SIX_AM sizeWithFont:_font];
+    CGFloat halfStrHeight = strSize.height / 2;
+    CGFloat tx4 = centerX - radius - halfLineWidth - GAP - strSize.width;
+    CGFloat ty4 = centerY - halfStrHeight;
+    [SIX_AM drawAtPoint:CGPointMake(tx4, ty4) withFont:_font];
+    
+    // top
+    strSize = [TWELVE_PM sizeWithFont:_font];
+    halfStrWidth = strSize.width / 2;
+    CGFloat tx5 = centerX - halfStrWidth;
+    CGFloat ty5 = centerY - radius - halfLineWidth - GAP - strSize.height;
+    [TWELVE_PM drawAtPoint:CGPointMake(tx5, ty5) withFont:_font];
+    
+    // right
+    strSize = [SIX_PM sizeWithFont:_font];
+    halfStrHeight = strSize.height / 2;
+    CGFloat tx6 = centerX + radius + halfLineWidth + GAP;
+    CGFloat ty6 = centerY - halfStrHeight;
+    [SIX_PM drawAtPoint:CGPointMake(tx6, ty6) withFont:_font];
 }
 
-- (void)drawClockWithStartAngle:(CGFloat)startAngle endAngle:(CGFloat)endAngle color:(UIColor*)color center:(CGPoint)center {
+- (void)drawClockWithStartAngle:(CGFloat)startAngle endAngle:(CGFloat)endAngle color:(UIColor*)color center:(CGPoint)center radius:(CGFloat)radius {
     
-    UIBezierPath *largeArcPath = [self createArcPathWithCenterPoint:center radius:LARGE_ARC_RADIUS startAngle:startAngle endAngle:endAngle];
+    UIBezierPath *largeArcPath = [self createArcPathWithCenterPoint:center radius:radius startAngle:startAngle endAngle:endAngle];
     [color setStroke];
     
     largeArcPath.lineWidth = STROKE_WDITH;
